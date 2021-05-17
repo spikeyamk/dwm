ff24cd2d9ecab97c16f7e05a9108f00a99f0c612
spikeyamk <76853812+spikeyamk@users.noreply.github.com>
2021-05-17 17:33:32 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 40;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Liberation Mono:size=18" };
-static const char dmenufont[]       = "Liberation Mono:size=18";
+static const char *fonts[]          = { "UbuntuMonoNerdFont:size=18" };
+static const char dmenufont[]       = "UbuntuMonoNerdFont:size=18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -65,14 +65,16 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, topbar ? NULL : "-b", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *qutebrowsercmd[] = { "qutebrowser", NULL };
+static const char *bravecmd[] = { "brave", NULL };
 /*static const char *rofi[] = { "rofi", "-show","run", NULL };*/
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,			XK_q,      spawn,	   {.v = qutebrowsercmd} },
+	{ MODKEY,			XK_q,      spawn,	   {.v = bravecmd} },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_r,	   spawn,          SHCMD("rofi -show run") },
+	{ MODKEY,			XK_s,	   spawn,	   SHCMD("setxkbmap sk") },
+	{ MODKEY,			XK_u,	   spawn,	   SHCMD("setxkbmap us") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
